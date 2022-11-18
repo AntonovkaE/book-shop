ae59394cf188735c1dbb8a8791bb173d8b8762cc
Elvira <sabirova.elvira.gam@gmail.com>
2022-11-18 13:07:03 +0500

    refactor: fix form validation

--- pages/order/validation.js
+++ pages/order/validation.js
@@ -1,19 +1,37 @@
+const stringChecking = new RegExp('\^[A-z]+$', '')
+const flatNumberChecking = new RegExp('\^[1-9-0]+$', '')
+const streetChecking  = new RegExp('[a-z1-9]', 'gi')
 const showInputError = (formElement, inputElement, errorMessage) => {
   const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
-  // inputElement.classList.add('form__input_type_error');
-  // errorElement.textContent = errorMessage;
-  // errorElement.classList.add('form__input-error_active');
+  inputElement.classList.add('form__input_type_error');
+  errorElement.textContent = errorMessage.length ? errorMessage : 'The field is invalid';
+  errorElement.classList.add('form__input-error_active');
 };
 
 const hideInputError = (formElement, inputElement) => {
   const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
-  // inputElement.classList.remove('form__input_type_error');
-  // errorElement.classList.remove('form__input-error_active');
-  // errorElement.textContent = '';
+  inputElement.classList.remove('form__input_type_error');
+  errorElement.classList.remove('form__input-error_active');
+  errorElement.textContent = '';
 };
 
+const checkInputContent = (inputElement) => {
+  console.log('check', inputElement)
+  if (inputElement.classList.contains('stringInput')) {
+    return stringChecking.test(inputElement.value)
+  } else if (inputElement.classList.contains('stringAndNumbersInput')) {
+    return streetChecking.test(inputElement.value)
+  } else if (inputElement.classList.contains('flatNumberInput')) {
+    if (inputElement.value[0] === '0' || inputElement.value[0] === '-') {
+      return  false
+    }
+    return flatNumberChecking.test(inputElement.value)
+  }
+  return true
+}
+
 const checkInputValidity = (formElement, inputElement) => {
-  if (!inputElement.validity.valid) {
+  if (!inputElement.validity.valid || !checkInputContent(inputElement)) {
     console.log(inputElement, inputElement.validationMessage)
     showInputError(formElement, inputElement, inputElement.validationMessage);
   } else {
